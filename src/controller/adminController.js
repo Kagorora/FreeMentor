f7230112edcbbac5e586f9361b160726f915e619
kagorora <kagororamaxime@gmail.com>
2019-08-19 12:33:46 -0700

    ft(update userType): Add update userType
    	- Add route for update userType
    	- add its API
    [Finishes #167984645]

---
+++ src/controller/adminController.js
@@ -0,0 +1,54 @@
+import userModal from '../model/users';
+
+class adminController {
+  static allUsers(req, res) {
+    if (req.user.userType === 'admin') {
+      return res.status(200).json({
+        status: 200,
+        data: userModal,
+      });
+    }
+    return res.status(403).json({
+      status: 403,
+      error: 'Forbirden route',
+    });
+  }
+
+  static changeUserType(req, res) {
+    if (req.user.userType === 'admin') {
+      const { userId } = req.params;
+      // eslint-disable-next-line radix
+      const foundUser = userModal.find(usr => usr.id === parseInt(userId));
+      if (foundUser && foundUser.userType === 'user') {
+        const updatedUser = {
+          id: foundUser.id,
+          firstName: foundUser.firstName,
+          lastName: foundUser.lastName,
+          email: foundUser.email,
+          password: foundUser.password,
+          address: foundUser.address,
+          bio: foundUser.bio,
+          occupation: foundUser.occupation,
+          expertise: foundUser.expertise,
+          userType: 'mentor',
+        };
+        userModal[userModal.indexOf(foundUser)] = updatedUser;
+        return res.status(200).json({
+          status: 200,
+          message: 'User account changed to mentor',
+          data: updatedUser,
+        });
+      }
+      return res.status(400).json(({
+        status: 400,
+        error: 'user is already a mentor or admin',
+      }));
+    }
+    return res.status(404).json({
+      status: 404,
+      error: 'Forbirden route',
+    });
+  }
+}
+
+export default adminController;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userController from '../controller/userController';
 import auth from '../authentification/auth';
+import adminController from '../controller/adminController';
 
 const app = express();
 
 app.post('/api/v1/auth/signup', userController.signUp);
 app.post('/api/v1/auth/signin', userController.signIn);
-app.get('/api/v1/auth/allUsers', auth, userController.allUsers);
+app.get('/api/v1/auth/allUsers', auth, adminController.allUsers);
+app.patch('/api/v1/auth/user/:userId', auth, adminController.changeUserType);
 
 export default app;
