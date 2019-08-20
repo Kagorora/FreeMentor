221e2734c83a3b7a0fac88d9dd26bf82422ee546
kagorora <kagororamaxime@gmail.com>
2019-08-19 22:56:44 -0700

    ft(session): create Session
    	- create route for creating session
    
    [Finishes #167992877]

---
+++ src/controller/sessionCOntroller.js
@@ -0,0 +1,30 @@
+import sessionModal from '../model/sessions';
+
+class sessionController {
+  static createSession(req, res) {
+    if (req.user.userType === 'user') {
+      const {
+        mentorId, questions,
+      } = req.body;
+      const sessionId = sessionModal.length + 1;
+      // eslint-disable-next-line no-undef
+      const newSession = {
+        id: sessionId, mentorId, menteeId: req.user.id, questions, menteeEmail: req.user.email, status: 'pending',
+      };
+      // eslint-disable-next-line no-undef
+      sessionModal.push(newSession.value);
+      return res.status(201).json({
+        status: 201,
+        data: {
+          id: sessionId, mentorId, menteeId: req.user.id, questions, menteeEmail: req.user.email, status: 'pending',
+        },
+      });
+    }
+    return res.status(403).json({
+      status: 403,
+      error: 'Only users can create mentorShip session',
+    });
+  }
+}
+
+export default sessionController;
